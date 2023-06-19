3be701f9966dada5c6ad50c6d07cad65ec75da46
jamie6264 <jamieturner6264@gmail.com>
2023-06-19 12:39:32 +0100

    serial data improvements

--- ESP/serial_data/serial_data.ino
+++ ESP/serial_data/serial_data.ino
@@ -7,18 +7,41 @@
 #define RX 16
 #define SERIAL_MODE SERIAL_8N1
 
-HardwareSerial SerialPort(2);
+//HardwareSerial SerialPort(2);
 
 void setup() {
-  Serial.begin(115200);  
-  SerialPort.begin(BAUD_RATE, SERIAL_MODE, RX, TX);
+  Serial.begin(BAUD_RATE);  
+  Serial2.begin(BAUD_RATE, SERIAL_MODE, RX, TX);
 }
-  uint8_t SerialBuffer[4];
-
+  //uint8_t SerialBuffer[4];
 
 void loop() {
-  if(SerialPort.available()){
-    Serial.print(SerialPort.read());
+  if(Serial2.available()){
+
+    uint32_t reievedFrame;
+
+    for(int i = 0; i<4; i++){
+      uint8_t byte = Serial2.read();
+      Serial.println("Byte number ", i+1);
+      Serial.println(byte);
+      recievedFrame |= (uint32_t)byte << (i * 8);
+    }
+    
+  }else{
+    Serial.println("not recieving");
+    delay(2000);
   }
+
+  Serial.println("The data recieved is:");
+  Serial.print(reciedFrame);
 }
+/* 
+need in json format::
+{
+  "xpos": num
+  "ypos": num
+  "orientation": angle
+  "lines": [0,1,0]      // corresponding to [wall on left, wall on right, wall in front]
+}
+*/
 
