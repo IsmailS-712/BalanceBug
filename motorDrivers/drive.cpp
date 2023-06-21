b53ae526b95ce11f85e849f4b2b1180781e21dc8
Kridhay <78971881+Kr1dhay@users.noreply.github.com>
2023-06-22 00:35:47 +0100

    drive system code

--- motorDrivers/drive.cpp
+++ motorDrivers/drive.cpp
@@ -26,8 +26,6 @@ void setup() {
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-
-
 // This section of code is the main loop
 //It constantly checks for UDP signals from the user and when received the rover's movement is controlled or the sensors are activated as required
 
@@ -35,74 +33,64 @@ void loop()
 {
   int packetSize = Udp.parsePacket();
   Serial.println("packet recieved");
-  if (packetSize)
-  {
-    Serial.print(F("Received packet of size "));
-    Serial.println(packetSize);
-    Serial.print(F("From "));
-    IPAddress remoteIp = Udp.remoteIP();
-    IPAddress ip(192, 168, 0, 197);
-    Serial.print(remoteIp);
-    Serial.print(F(", port "));
-    Serial.println(Udp.remotePort());
 
-    // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 255);
-    if (len > 0)
-    {
-      packetBuffer[len] = 0;
-    }
-    // print content of packet received by rover
-    Serial.println(F("Contents:"));
-    Serial.println(packetBuffer);
-    // execute command based on packet received
-    if (packetBuffer[0] == 'w'){ //slow forward
-      analogWrite(LPWM,50);
-      analogWrite(RPWM,50);
-      digitalWrite(LDIR,1);
-      digitalWrite(RDIR,1);
-    }
-    else if(packetBuffer[0] == 's'){ //slow backward
-      analogWrite(LPWM,50);
-      analogWrite(RPWM,50);
-      digitalWrite(LDIR,0);
-      digitalWrite(RDIR,0);
-    }
-    else if(packetBuffer[0] == 'a'){ //slow left
-      analogWrite(LPWM,20);
-      analogWrite(RPWM,20);
-      digitalWrite(LDIR,0);
-      digitalWrite(RDIR,1);
-    }
-    else if(packetBuffer[0] == 'd'){ //slow right
-      analogWrite(LPWM,20);
-      analogWrite(RPWM,20);
-      digitalWrite(LDIR,1);
-      digitalWrite(RDIR,0);
-    }
-    else if(packetBuffer[0] == '1'){ // stop (executed upon key release)
-      analogWrite(LPWM,0);
-      analogWrite(RPWM,0);
-      digitalWrite(LDIR,0);
-      digitalWrite(RDIR,0);
-      Udp.beginPacket(ip, 60472);
-      String rock = String(rockDetected());
-      Udp.print("Rocks");
-      
-      Udp.endPacket();
-    }
-    else if(packetBuffer[0] == 'f'){ // detect
-      Udp.beginPacket(ip, 60472);
-      String rock = String(rockDetected());
-      Udp.print(rock);
-      
-      Udp.endPacket();
-    }
-    // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), 55125);
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
 
-    Udp.write("RPLY");
-    
-    Udp.endPacket();
+    // Send GET request
+    http.begin(serverName);
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+
+      // Parse JSON data
+      StaticJsonDocument<200> jsonDocument;
+      DeserializationError error = deserializeJson(jsonDocument, payload);
+
+      if (error) {
+        Serial.println("Failed to parse JSON");
+      } else {
+        // Access JSON data
+        int value = jsonDocument["value"];
+
+        // Process the JSON data as needed
+        if (value == 'fwd'){ //slow forward
+          analogWrite(LPWM,50);
+          analogWrite(RPWM,50);
+          digitalWrite(LDIR,1);
+          digitalWrite(RDIR,1);
+        } else if(value == 'bck'){ // backward
+          analogWrite(LPWM,50);
+          analogWrite(RPWM,50);
+          digitalWrite(LDIR,0);
+          digitalWrite(RDIR,0);
+        } else if(value == 'lft'){ // left
+          analogWrite(LPWM,20);
+          analogWrite(RPWM,20);
+          digitalWrite(LDIR,0);
+          digitalWrite(RDIR,1);
+        } else if(value == 'rht'){ // right
+          analogWrite(LPWM,20);
+          analogWrite(RPWM,20);
+          digitalWrite(LDIR,1);
+          digitalWrite(RDIR,0);
+        } else if(value == 'stop'){ // stop
+          analogWrite(LPWM,0);
+          analogWrite(RPWM,0);
+          digitalWrite(LDIR,0);
+          digitalWrite(RDIR,0);
+        }
+
+        // Udp.beginPacket(Udp.remoteIP(), 55125);
+        // Udp.write("RPLY");
+      }
+    } else {
+      Serial.print("HTTP request failed with error code: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
   }
+
 }
